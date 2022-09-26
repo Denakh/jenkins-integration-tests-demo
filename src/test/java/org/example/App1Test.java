9ab943b654a2f2c3a0738d5c09f1efda5f27e676
practise_un_denakh <denakhokh@gmail.com>
2022-09-26 10:27:12 +0300

    Some

---
+++ src/test/java/org/example/App1Test.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class App1Test {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
---
+++ src/test/java/org/example/App3Test.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class App3Test {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue3() {
+        assertTrue(true);
+    }
+}
